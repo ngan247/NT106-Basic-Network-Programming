d81f7a428e22e365dcb13bb311cc27e4cd3cf782
Nguyễn Bùi Kim Ngân <59002413+ngan247@users.noreply.github.com>
2023-07-12 15:06:23 +0700

    Add files via upload

---
+++ Project/Source_code/test/test/room.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public class room
+    {
+        private string name;
+        private string address;
+        private int port;
+        private bool connect;
+        private connector myConnectRoom;
+
+        public room(string name, string address, int port)
+        {
+            this.name = name;
+            this.address = address;
+            this.port = port;
+            this.connect = false;
+            this.myConnectRoom = new connector();
+        }
+
+        public string nameGetSet
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string addressGetSet
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+
+        public int portGetSet
+        {
+            get
+            {
+                return port;
+            }
+            set
+            {
+                port = value;
+            }
+        }
+
+        public bool connectGetSet
+        {
+            get
+            {
+                return connect;
+            }
+            set
+            {
+                connect = value;
+            }
+        }
+
+        public connector myConnectRoomGetSet
+        {
+            get
+            {
+                return myConnectRoom;
+            }
+            set
+            {
+                myConnectRoom = value;
+            }
+        }
+    }
+}
